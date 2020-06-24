82d6302182467bad6a6f659a1f7212207aea2704
sky5972069 <38596562+sky5972069@users.noreply.github.com>
2020-06-24 18:16:08 +0800

    persons_first

--- index.js
+++ index.js
@@ -1,88 +1,97 @@
 const express = require('express')
+// const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
-// app.use(express.static('build'))
+app.use(express.static('build'))
 
-let notes = [
+// morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
+// morgan.format('joke', ':method :url :status :type');
+// app.use(morgan('joke'))
+let persons = [
   {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
+    "name": "Arto Hellas",
+    "number": "040-1234567",
+    "id": 1
   },
   {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
   },
   {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
   }
 ]
-
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+              ${Date()}`)
   })
   
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
   })
   
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(n=>n.id===id)
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p=>p.id!==id)
+    res.status(204).end()
+  })
+
+
+const generateId = () => {
+    const Id = Math.ceil(Math.random()*100000)
+
+    return Id
+  }
+
+
+app.post('/api/persons', (req, res) => {
+      const newPerson = req.body
+      if (!newPerson.name) {
+        return res.status(400).json({error:'name missing'})
+      } else if (!newPerson.number) {
+        return res.status(400).json({error:'number missing'})
+      } else if ([...persons.map(p=>p.name)].find(p=>p.name===newPerson.name)){
+        return res.status(400).json({error:'name already exists'})
+      } else {
+        const newP = {
+          name: newPerson.name,
+          number : newPerson.number,
+          id: generateId()
+        }
+        persons = persons.concat(newP)
+        res.json(newP)
+      }
+
+  })
+
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-      } else {
-        console.log(note)
-        response.status(404).end()
-      }
-  })
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
-
-
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
-  })
